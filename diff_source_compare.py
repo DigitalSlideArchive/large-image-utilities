6ebb5bd9955b58b26d254ab6abddc864b8551b90
David Manthey <david.manthey@kitware.com>
2023-05-25 00:15:39 -0400

    Refactor diff_source_compare

--- diff_source_compare.py
+++ diff_source_compare.py
@@ -7,7 +7,42 @@
 
 import argparse
 import re
-import sys
+
+
+def parse_file(path):
+    records = {}
+    src = None
+    with open(path) as fptr:
+        lastline = None
+        source = None
+        style = None
+        projection = None
+        entry = {'sources': {}}
+        for line in fptr:
+            if line.startswith('Source '):
+                if source:
+                    entry['sources'].pop((style, projection, source))
+                if src:
+                    records[src] = entry
+                source = None
+                if lastline:
+                    src = lastline
+                entry = {'header1': line, 'sources': {}}
+            elif line.startswith('mag '):
+                entry['header2'] = line
+            elif line.startswith('Style: '):
+                style = line
+            elif line.startswith('Projection: '):
+                projection = line
+            elif line[:1] != ' ' and line[:1] > '9':
+                source = line.split()[0]
+                entry['sources'][(style, projection, source)] = {'line1': line}
+            else:
+                entry['sources'][(style, projection, source)]['line2'] = line
+            lastline = line
+        if src and len(entry):
+            records[src] = entry
+    return records
 
 
 def command():  # noqa
@@ -19,108 +54,60 @@ def command():  # noqa
     parser.add_argument('file1', help='First text file')
     parser.add_argument('file2', help='Second text file')
     opts = parser.parse_args()
-    f1 = open(opts.file1)
-    f2 = open(opts.file2)
-    source1 = None
-    source2 = None
-    # style1 = None
-    # style2 = None
-    # projection1 = None
-    # projection2 = None
-    line1 = None
-    line2 = None
-    diffsource = None
-    diff1 = []
-    diff2 = []
-    even = False
-    diff = False
-    lastdiff = False
-    while True:
-        lastline1 = line1
-        lastline2 = line2
-        lastdiff = diff
-        try:
-            line1 = next(f1)
-        except Exception:
-            line1 = None
-        try:
-            line2 = next(f2)
-        except Exception:
-            line2 = None
-        if not line1 and not line2:
-            break
-        even = not even
-        line1 = line1 or ''
-        line2 = line2 or ''
-        if line1.startswith('Source '):
-            source1 = lastline1
-            sys.stdout.write(''.join(diff1) + ''.join(diff2))
-            diff1 = []
-            diff2 = []
-            diffsource = None
-            even = True
-        if line2.startswith('Source '):
-            source2 = lastline2
-            sys.stdout.write(''.join(diff1) + ''.join(diff2))
-            diff1 = []
-            diff2 = []
-            diffsource = None
-        if line1.startswith('Style: '):
-            # style1 = lastline1
-            sys.stdout.write(''.join(diff1) + ''.join(diff2))
-            diff1 = []
-            diff2 = []
-            diffsource = None
-        if line2.startswith('Style: '):
-            # style2 = lastline2
-            sys.stdout.write(''.join(diff1) + ''.join(diff2))
-            diff1 = []
-            diff2 = []
-            diffsource = None
-        if line1.startswith('Projection: '):
-            # projection1 = lastline1
-            sys.stdout.write(''.join(diff1) + ''.join(diff2))
-            diff1 = []
-            diff2 = []
-            diffsource = None
-        if line2.startswith('Projection: '):
-            # projection2 = lastline2
-            sys.stdout.write(''.join(diff1) + ''.join(diff2))
-            diff1 = []
-            diff2 = []
-            diffsource = None
-        diff = False
-        if line1 != line2:
-            parts1 = line1.split()
-            parts2 = line2.split()
-            if len(parts1) != len(parts2):
-                diff = True
-            else:
-                for idx in range(len(parts1)):
-                    if parts1[idx] != parts2[idx]:
-                        if (not re.match(r'\d+\.\d+s', parts1[idx]) or
-                                not re.match(r'\d+\.\d+s', parts2[idx])):
-                            diff = True
-                        else:
-                            time1 = float(parts1[idx][:-1])
-                            time2 = float(parts2[idx][:-1])
-                            if abs(time1 - time2) > 0.1 and (
-                                    time1 / time2 if time2 > time1 else time2 / time1) < 0.5:
+    source1 = parse_file(opts.file1)
+    source2 = parse_file(opts.file2)
+
+    records = set(source1.keys()) | set(source2.keys())
+    for record in sorted(records):
+        if record not in source1:
+            print('>%s' % record.rstrip())
+            continue
+        if record not in source2:
+            print('<%s' % record.rstrip())
+            continue
+        sources = set(source1[record]['sources']) | set(source2[record]['sources'])
+        lastprint = None
+        for style, projection, source in sorted(sources):
+            s1 = source1[record]['sources'].get((style, projection, source))
+            s2 = source2[record]['sources'].get((style, projection, source))
+            # print(s1, s2)
+            diff = s1 is None or s2 is None
+            if not diff:
+                parts11 = s1['line1'].split()
+                parts12 = s1['line2'].split()
+                parts21 = s2['line1'].split()
+                parts22 = s2['line2'].split()
+                for parts1, parts2 in ((parts11, parts21), (parts12, parts22)):
+                    diff = diff or len(parts1) != len(parts2)
+                    for idx in range(min(len(parts1), len(parts2))):
+                        if parts1[idx] != parts2[idx]:
+                            if (not re.match(r'\d+\.\d+s', parts1[idx]) or
+                                    not re.match(r'\d+\.\d+s', parts2[idx])):
                                 diff = True
-        if diff and not diffsource:
-            if source1 != source2:
-                diff1.append('<%s\n' % source1.rstrip())
-                diff2.append('>%s\n' % source2.rstrip())
-            else:
-                diff1.append(' %s\n' % source1.rstrip())
-            diffsource = source1, source2
-        if diff:
-            if not even and not lastdiff:
-                diff1.append('<%s\n' % lastline1.rstrip())
-                diff2.append('>%s\n' % lastline2.rstrip())
-            diff1.append('<%s\n' % line1.rstrip())
-            diff2.append('>%s\n' % line2.rstrip())
-    sys.stdout.write(''.join(diff1) + ''.join(diff2))
+                            else:
+                                time1 = float(parts1[idx][:-1])
+                                time2 = float(parts2[idx][:-1])
+                                if abs(time1 - time2) > 0.1 and (
+                                        time1 / time2 if time2 > time1 else time2 / time1) < 0.5:
+                                    diff = True
+            if diff:
+                if not lastprint:
+                    print(' %s' % record.strip())
+                    print(' %s' % source1[record]['header1'].rstrip())
+                    print(' %s' % source1[record]['header2'].rstrip())
+                    lastprint = [-1, -1]
+                if lastprint[0] != style:
+                    if style is not None:
+                        print(' %s' % style.rstrip())
+                    lastprint[0] = style
+                if lastprint[1] != projection:
+                    if projection is not None:
+                        print(' %s' % projection.rstrip())
+                    lastprint[1] = projection
+                print('<%s' % (s1['line1'].rstrip() if s1 else ''))
+                print('<%s' % (s1['line2'].rstrip() if s1 else ''))
+                print('>%s' % (s2['line1'].rstrip() if s2 else ''))
+                print('>%s' % (s2['line2'].rstrip() if s2 else ''))
 
 
 if __name__ == '__main__':
