360a4cb3a5e9641f99e9202ff934179b0caface9
David Manthey <david.manthey@kitware.com>
2023-05-30 13:49:04 -0400

    More fake data options

--- fake_slide_data.py
+++ fake_slide_data.py
@@ -3,12 +3,16 @@
 # pip install girder_client faker_biology
 
 import argparse
+import copy
 import os
 import random
 import time
 
 import faker
+import faker_biology.bioseq
+import faker_biology.mol_biol
 import faker_biology.physiology
+import faker_biology.taxonomy
 import girder_client
 import girder_client.cli
 
@@ -32,11 +36,42 @@ def get_girder_client(opts):
 def make_func(fake, value, fieldtype, typeval):
     if hasattr(fake, value):
         return getattr(fake, value)
+    typeval = copy.copy(typeval)
+    if fieldtype in {'range', 'floatrange'} and typeval[0].startswith('random'):
+        typeval[0] = random.uniform(0, float(typeval[0].split('random')[1]))
+    if fieldtype in {'range', 'floatrange'} and typeval[1].startswith('random'):
+        typeval[1] = random.uniform(float(typeval[0]), float(typeval[1].split('random')[1]))
     return {
         'range': lambda: random.randint(int(typeval[0]), int(typeval[1])),
         'floatrange': lambda: random.uniform(float(typeval[0]), float(typeval[1])),
         'enum': lambda: random.choice(typeval)
     }[fieldtype]
+
+
+def create_items(client, source, count, folder, fake):
+    count = int(count)
+    try:
+        req = client.get('item', {'folderId': folder}, jsonResp=False)
+        count -= int(req.headers['Girder-Total-Count'])
+    except Exception:
+        pass
+    if count <= 0:
+        return
+    itemIds = list({'_id': item['_id'], 'name': item['name']}
+                   for item in client.listItem(source))
+    lasttime = 0
+    while count > 0:
+        if time.time() - lasttime > 10:
+            print('Adding %d item(s)' % count)
+            lasttime = time.time()
+        item = random.choice(itemIds)
+        name = fake.iban() + '.' + item['name'].split('.')[-1]
+        client.post(f'item/{item["_id"]}/copy', parameters={
+            'folderId': folder,
+            'name': name,
+            'description': 'Fake item',
+        })
+        count -= 1
 
 
 if __name__ == '__main__':  # noqa
@@ -75,10 +110,25 @@ if __name__ == '__main__':  # noqa
         '--folder', '--id', dest='folder',
         help='The Girder folder id to process')
     parser.add_argument(
+        '--count',
+        help='The number of items that should exist in the folder.  Use with '
+        'the <source> option to copy additional items with fake names to the '
+        'specified folder.')
+    parser.add_argument(
+        '--source',
+        help='The Girder source folder id to copy items from to make <count> '
+        'fake items.')
+    parser.add_argument(
         '--field', action='append',
         help='(Field name):(Field Type).  Field type is one of "organelle", '
         '"celltype", "range:(low),(high)", "floatrange:(low),(high)", '
-        '"enum:(comma separated list)".')
+        '"enum:(comma separated list)".  For range and floatrange, low and '
+        'high can be numbers or "random<number>" in which case they will be a '
+        'random value between [0, <number>] or [<low>, <number>].  If field '
+        'name includes "[(low)-(high)[-(step)]" or [(char1)-(char2)], a '
+        'series of fields will be created with that range of integers or '
+        'characters; for integers, the range follows python rules; for '
+        'characters it includes char2.')
     parser.add_argument(
         '--annotation', help='(chance[0-1]),(min points),(max points),(iterations)')
 
@@ -89,6 +139,12 @@ if __name__ == '__main__':  # noqa
     fake.add_provider(faker_biology.physiology.CellType)
     fake.add_provider(faker_biology.physiology.Organ)
     fake.add_provider(faker_biology.physiology.Organelle)
+    fake.add_provider(faker_biology.bioseq.Bioseq)
+    fake.add_provider(faker_biology.mol_biol.RestrictionEnzyme)
+    fake.add_provider(faker_biology.mol_biol.Antibody)
+    fake.add_provider(faker_biology.mol_biol.Enzyme)
+    fake.add_provider(faker_biology.taxonomy.ModelOrganism)
+
     annot = None
     if args.annotation:
         annot = args.annotation.split(',')
@@ -99,10 +155,24 @@ if __name__ == '__main__':  # noqa
         fieldtype, typeval = value.split(':', 1) if ':' in value else (value, None)
         if typeval:
             typeval = typeval.split(',')
-        fields[key] = make_func(fake, value, fieldtype, typeval)
-
+        if '[' not in key:
+            fields[key] = make_func(fake, value, fieldtype, typeval)
+        else:
+            keypart1, rangepart = key.split('[', 1)
+            rangepart, keypart2 = rangepart.split(']', 1)
+            rangepart = rangepart.split('-')
+            try:
+                rangepart = range(*(int(part) for part in rangepart))
+            except Exception:
+                rangepart = [chr(r) for r in range(ord(rangepart[0]), ord(rangepart[1]) + 1)]
+            for r in rangepart:
+                rkey = keypart1 + str(r) + keypart2
+                fields[rkey] = make_func(fake, value, fieldtype, typeval)
     client = get_girder_client(vars(args))
-    for item in client.listItem(args.folder):
+    print('%d field(s) of metadata' % (len(fields), ))
+    if args.count and args.source:
+        create_items(client, args.source, args.count, args.folder, fake)
+    for item in client.listResource('item', {'folderId': args.folder, 'sort': 'updated'}):
         print(item['name'])
         try:
             limetadata = client.get('item/%s/tiles' % item['_id'])
