b421f7e8eb919061e94becb748d3bf540df80ec5
David Manthey <david.manthey@kitware.com>
2024-05-16 08:59:17 -0400

    More console image options

--- console_image.py
+++ console_image.py
@@ -2,7 +2,9 @@
 
 import argparse
 import os
+import subprocess
 import sys
+import tempfile
 
 import large_image
 import numpy as np
@@ -25,6 +27,30 @@ def to_dots(clr, bw, usecolor):
     return out
 
 
+def to_blocks(blocks, usecolor, x, vblocks):
+    fac = [0.3, 0.59, 0.11]
+    hdist = sum((float(blocks[1][0][idx]) - blocks[0][0][idx]) ** 2 * fac[idx]
+                for idx in range(3))
+    vdist = sum((float(vblocks[0][1][idx]) - vblocks[0][0][idx]) ** 2 * fac[idx]
+                for idx in range(3))
+    # The factor after vdist makes it more pleasing even though technically
+    # lower in color resolution
+    if hdist <= vdist * 4:
+        out = (f'\033[48;2;{blocks[0][0][0]};{blocks[0][0][1]};{blocks[0][0][2]}m'
+               f'\033[38;2;{blocks[1][0][0]};{blocks[1][0][1]};{blocks[1][0][2]}m' +
+               '\u2584')
+    else:
+        out = (f'\033[48;2;{vblocks[0][0][0]};{vblocks[0][0][1]};{vblocks[0][0][2]}m'
+               f'\033[38;2;{vblocks[0][1][0]};{vblocks[0][1][1]};{vblocks[0][1][2]}m' +
+               '\u2590')
+    if usecolor is not None:
+        if usecolor.get('last', None) == out and x:
+            out = usecolor['last'][-1:]
+        else:
+            usecolor['last'] = out
+    return out
+
+
 def main(opts):
     try:
         termw, termh = os.get_terminal_size()
@@ -35,10 +61,17 @@ def main(opts):
     if opts.height:
         termh = opts.height
 
+    eight = False
     width = termw * 2
     height = termh * 4
-    aspect_ratio = 0.55 * 2
+    # aspect_ratio = 0.55 * 2
+    aspect_ratio = 0.5 * 2
     color = opts.color
+
+    if opts.tiv:
+        width = height = max(termw, termh) * 8
+        aspect_ratio = 1
+    eight = opts.dots or not opts.color
 
     thumbw = width if aspect_ratio < 1 else int(width * aspect_ratio)
     thumbh = height if aspect_ratio > 1 else int(height / aspect_ratio)
@@ -58,27 +91,52 @@ def main(opts):
         charw, charh = thumbw // 2, int(thumbh * aspect_ratio) // 4
     else:
         charw, charh = int(thumbw / aspect_ratio) // 2, thumbh // 4
-    dotw, doth = charw * 2, charh * 4
+    if eight:
+        dotw, doth = charw * 2, charh * 4
+    else:
+        dotw, doth = charw, charh * 2
 
     adjimg = PIL.ImageOps.autocontrast(img, cutoff=0.02)
     # adjimg = PIL.ImageOps.equalize(img)
     img = PIL.Image.blend(img, adjimg, opts.contrast)
 
-    dotimg = img.resize((dotw, doth)).convert('1', dither=False)
-    charimg = img.convert('RGB').resize((charw, charh))
+    if opts.tiv:
+        with tempfile.TemporaryDirectory() as tmpdir:
+            tmpfile = os.path.join(tmpdir, 'thumb.png')
+            img.save(tmpfile)
+            output = subprocess.check_output([
+                'tiv', '-w', str(termw), '-h', str(termh), '-0', tmpfile]).decode()
+            output = output.rstrip()
+        return output
 
-    dots = np.array(dotimg)
-    chars = np.array(charimg)
+    if eight:
+        dotimg = img.resize((dotw, doth)).convert('1', dither=False)
+        charimg = img.convert('RGB').resize((charw, charh))
 
-    lastcolor = {} if color else None
+        dots = np.array(dotimg)
+        chars = np.array(charimg)
 
-    output = [
-        [to_dots(chars[y][x], dots[y * 4:y * 4 + 4, x * 2:x * 2 + 2], lastcolor)
-         for x in range(chars.shape[1])]
-        for y in range(chars.shape[0])
-    ]
+        lastcolor = {} if color else None
 
-    output = '\n'.join(''.join(line) for line in output)
+        output = [
+            [to_dots(chars[y][x], dots[y * 4:y * 4 + 4, x * 2:x * 2 + 2], lastcolor)
+             for x in range(chars.shape[1])]
+            for y in range(chars.shape[0])
+        ]
+    else:
+        blockimg = np.array(img.convert('RGB').resize((dotw, doth)))
+        vblockimg = np.array(img.convert('RGB').resize((dotw * 2, doth // 2)))
+
+        lastcolor = {} if color else None
+
+        output = [
+            [to_blocks(blockimg[y:y + 2, x:x + 1], lastcolor, x,
+                       vblockimg[y // 2: y // 2 + 1, x * 2: x * 2 + 2])
+             for x in range(blockimg.shape[1])]
+            for y in range(0, blockimg.shape[0], 2)
+        ]
+
+    output = '\033[39m\033[49m\n'.join(''.join(line) for line in output)
     if color:
         output += '\033[39m\033[49m'
     return output
@@ -106,6 +164,12 @@ def command():
         help='Increase the contrast.  0 is no change, 1 is full.')
     parser.add_argument(
         '--use', help='Use a specific tile source.')
+    parser.add_argument(
+        '--dots', action='store_true', default=False,
+        help='Use small dots for outputting the image.')
+    parser.add_argument(
+        '--tiv', action='store_true', default=False,
+        help='Use tiv for outputting the image.')
     opts = parser.parse_args()
     result = main(opts)
     if result:
